1a7c96390d6886d27bd2906dd31ff80bffed4499
p-karbownik <baciar112@gmail.com>
2023-01-15 20:07:12 +0100

    added charts for stage2

--- ploting/time.py
+++ ploting/time.py
@@ -2,10 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def create_time_chart(supports: list, time: list, title: str, plot_path: str, plot_format: str):
+def create_time_chart(supports_stage1: list, time_stage1: list, supports_stage2: list, time_stage2: list, title: str,
+                      plot_path: str,
+                      plot_format: str):
     plt.clf()
-    plt.plot(supports, time)
+    plt.plot(supports_stage1, time_stage1, label="noBitArray")
+    plt.plot(supports_stage2, time_stage2, label="bitArray")
     plt.title(title)
+    plt.legend()
     plt.xlabel("Support")
     plt.ylabel("Execution time [ms]")
 
@@ -20,22 +24,40 @@ def draw_time_charts():
                          "politics"]
 
     for n in time_charts_names:
-        df = pd.read_csv("./results/time/" + n + ".csv")
-        df.round({'time': 0})
-        create_time_chart(df['support'], df['time'], "Execution time for dataset " + n + ".csv", "./results/time"
-                                                                                                 "/plots/" + n +
+        df_stage1 = pd.read_csv("./results/time/" + n + ".csv")
+        df_stage1.round({'time': 0})
+
+        df_stage2 = pd.read_csv("./results/time_stage2/" + n + ".csv")
+        df_stage2.round({'time': 0})
+
+        create_time_chart(df_stage1['support'], df_stage1['time'], df_stage2['support'], df_stage2['time'],
+                          "Execution time for dataset " + n + ".csv", "./results/time"
+                                                                      "/plots/" + n +
                           ".png", "PNG")
 
         if n != "muzyka":
-            n_df = df.iloc[1:, :]
-
-            create_time_chart(n_df['support'], n_df['time'], "Execution time for dataset " + n + ".csv", "./results/time"
-                                                                                                     "/plots/" + n +
+            n_df = df_stage1.iloc[1:, :]
+            n_df2 = df_stage2.iloc[1:, :]
+            create_time_chart(n_df['support'], n_df['time'], n_df2['support'], n_df2['time'],
+                              "Execution time for dataset " + n + ".csv",
+                              "./results/time"
+                              "/plots/" + n +
                               "2.png", "PNG")
+            if n == "movies":
+                n_df = n_df.iloc[2:, :]
+                n_df2 = n_df2.iloc[2:, :]
+
+                create_time_chart(n_df['support'], n_df['time'], n_df2['support'], n_df2['time'],
+                                  "Execution time for dataset " + n + ".csv",
+                                  "./results/time"
+                                  "/plots/" + n +
+                                  "3.png", "PNG")
             if n == "cinema":
                 n_df = n_df.iloc[3:, :]
+                n_df2 = n_df2.iloc[3:, :]
 
-                create_time_chart(n_df['support'], n_df['time'], "Execution time for dataset " + n + ".csv",
+                create_time_chart(n_df['support'], n_df['time'], n_df2['support'], n_df2['time'],
+                                  "Execution time for dataset " + n + ".csv",
                                   "./results/time"
                                   "/plots/" + n +
                                   "3.png", "PNG")
